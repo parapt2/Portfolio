8e5b8537266526fbabd2dc6bac3d4be1518803de
3mgb3000 <3mgb3000@proton.me>
2026-02-27 15:34:15 +0000

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import "/src/sass/style.scss";
