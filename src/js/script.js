99ecf731e45fa6e639b2e9c1512b8c8b2a904d41
3mgb3000 <3mgb3000@proton.me>
2026-03-02 17:18:19 +0000

    step 1-2 first screen

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,16 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import "/src/sass/style.scss";
+
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
