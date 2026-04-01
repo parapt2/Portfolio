a3f6933228d26f2dc86cc4ebfa13db26730e7ff6
3mgb3000 <3mgb3000@proton.me>
2026-04-01 15:14:40 +0100

    personalization form validation & submission

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
 import JustValidate from 'just-validate';
 
 // import 'swiper/css';
@@ -28,3 +28,77 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+try {
+    const validatorTouch = new JustValidate('.contacts__form', {
+        errorLabelStyle: {
+            paddingTop: '3px',
+            color: '#d6247c',
+            fontSize: '10px',
+        },
+        errorFieldCssClass: 'input-error',
+    });
+
+    validatorTouch
+        .addField('#name', [
+            {
+                rule: 'required',
+                // errorMessage: "Please fill the name"
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                // errorMessage: "Minimum 2 chars!"
+            },
+        ])
+        .addField('#email', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'email',
+            },
+        ])
+        .addField('#message', [
+            {
+                rule: 'required',
+            },
+            {
+                rule: 'minLength',
+                value: 5,
+            },
+        ], {
+            errorsContainer: document.querySelector('#message').parentElement.querySelector('.error-message'),
+        })
+        .addField('#checkbox', [
+            {
+                rule: 'required',
+            },
+        ], {
+            errorsContainer: document.querySelector('#checkbox').parentElement.parentElement.querySelector('.checkbox-error-message'),
+        })
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch("https://formspree.io/f/mpqolloz", {
+                method: "POST",
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+                .then((res) => {
+                    if (res.ok) {
+                        form.reset();
+                        alert("Message sent!");
+                    } else {
+                        alert("Something went wrong 😢");
+                    }
+                })
+                .catch(() => {
+                    alert("Network error");
+                });
+        });
+} catch (e) { }
